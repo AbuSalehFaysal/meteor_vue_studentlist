2f7619ef481af0ff49390d1d7965c773b9ee9695
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2020-12-28 00:06:39 +0600

    Project Upload

---
+++ imports/ui/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <br />
+    <div>
+  <b-navbar toggleable="lg" type="dark" variant="success">
+    <b-navbar-brand to="/home">NavBar</b-navbar-brand>
+    <b-navbar-brand to="/add-student">Add Student</b-navbar-brand>
+    <b-navbar-brand to="/add-subject">Add Subject</b-navbar-brand>
+    <b-navbar-brand to="/assign-subject">Assign Subject</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+  </b-navbar>
+</div>
+    <br />
+    <div>
+      <router-view></router-view>
+    </div>
+
+    <br />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Home from "./components/Home";
+export default {
+  components: {
+    Home,
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
